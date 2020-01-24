977b698200c2c911bbbd91b771560af0507ee4a8
Havi Nguyen <havinguyen777@gmail.com>
2020-01-23 21:38:27 -0500

    background update

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
 function setup() {
-	createCanvas(800,600); // make an HTML canvas element width x height pixels
+	createCanvas(800,600); 
 }
 
 function draw() {
-	background(225);
+	background(255, 204, 0);
 	textSize(32);
-	fill(180);
+	fill(255, 204, 0);
 	text(hour(), 10, 30);
 	fill(100);
 	text(minute(), 10, 60);
 	fill(0);
 	text(second(), 10, 90);
+	console.log(minute()); 
 }
